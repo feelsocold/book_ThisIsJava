3637eec2ca4bf5edb2f761dd20aaf110e9fef958
feelsocold <feelsocoold@gmail.com>
2020-01-10 23:06:33 +0900

    IO

---
+++ book_ThisIsJava/src/CHAPTER18_IO/example01_Console/SystemInExample1.java
@@ -0,0 +1,37 @@
+package CHAPTER18_IO.example01_Console;
+
+import java.io.InputStream;
+
+public class SystemInExample1 {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("--메뉴--");
+		System.out.println("1. 예금 조회");
+		System.out.println("2. 예금 출금");
+		System.out.println("3. 예금 입금");
+		System.out.println("4. 종료 하기");
+		System.out.println("메뉴를 선택하세요");
+		
+		System.out.print("\n입력 : ");
+		InputStream is = System.in;
+		
+		char inputChar = (char) is.read();	// 아스키 코드로 읽고 문자로 return
+		
+		switch(inputChar) {
+			case '1' :
+				System.out.println("예금 조회를 선택하셨습니다.");
+				break;
+			case '2' :
+				System.out.println("예금 출금을 선택하셨습니다.");
+				break;
+			case '3' :
+				System.out.println("예금 입금을 선택하셨습니다.");
+				break;
+			case '4' :
+				System.out.println("종료 하기를 선택하셨습니다.");
+				break;
+		}
+		
+	}
+
+}
